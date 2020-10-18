c528c75991f127d4757b59a333169129466940f5
julianoprogrammierer <julianoprogrammierer@gmail.com>
2020-10-18 19:59:56 -0300

    NLW3 =D

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -1,27 +1,30 @@
 const options = {
-    ragging : false,
+    dragging: false,
     touchZoom: false,
-    doubleClickZoom : false,
-    scrollWheelZoom : false,
-    zoomControl : false,
-
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false,
 }
 
+const map = L.map('mapid', options).setView([-27.2109325,-49.6448719], 15);
 
-const map = L.map('mapid', options).setView([-27.645618, -48.667854], 15);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',)
+.addTo(map);
 
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
+    iconUrl: "/images/map-marker.svg",
     iconSize: [58, 68],
-    iconAnchor:[29, 68],
-    popupAnchor: [170, 2]
+    iconAnchor: [29,68],
+    popAnchor: [170, 2]
 })
 
-L.marker([-27.645618, -48.667854], { icon })
+L
+.marker([-27.2109325,-49.6448719], { icon })
 .addTo(map)
-/*gallery */
+
+
+/* image gallery */
+
 function selectImage(event) {
     const button = event.currentTarget
 
--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,22 +1,38 @@
-const map = L.map('mapid').setView([-27.645618, -48.667854], 15);
+const map = L.map('mapid').setView([-27.2109325,-49.6448719], 15);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',)
+.addTo(map);
 
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
+    iconUrl: "/images/map-marker.svg",
     iconSize: [58, 68],
-    iconAnchor:[29, 68],
+    iconAnchor: [29,68],
     popupAnchor: [170, 2]
 })
 
-const popup = L.popup({
-    closeButton: false,
-    className: 'map-popup',
-    minWidth: 240,
-    minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orfanage"> <img src="./public/images/arrow-white.svg"> </a>')
-
-L.marker([-27.645618, -48.667854], { icon })
-.addTo(map)
-.bindPopup(popup)
+function addMarker({id, name, lat, lng } = orphanage) {
+    const popup = L.popup({
+        closeButton: false,
+        className: "map-popup",
+        minWidth: 240,
+        minHeight: 240
+    }).setContent(`${name} <a href="/orphanage?id=${id}"> <img src="/images/arrow-white.svg"> </a>`)
     
+    L
+    .marker([lat, lng], { icon })
+    .addTo(map)
+    .bindPopup(popup)    
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span')
+
+orphanagesSpan.forEach( span => {
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng,
+    }
+
+    addMarker(orphanage)
+})
